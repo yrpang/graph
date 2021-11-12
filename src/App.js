ca4cab2e5c4ff08072f7d95d6ead1154d4e47621
huangzhuo <huangzhuo.1226@bytedance.com>
2021-11-12 19:20:37 +0800

    feat: 路由鉴权

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import Index from './pages/Index'
+import Home from './pages/Home'
+import 'antd/dist/antd.css'
+import { BrowserRouter as Router, Switch, Route, Redirect, useLocation } from 'react-router-dom'
+
+const routeConfig = [
+  {
+    path: '/index',
+    component: Index,
+    auth: false,
+  },
+  {
+    path: '/home',
+    component: Home,
+    auth: true,
+  },
+]
+
+function FrontedAuth() {
+  const location = useLocation()
+  const result = routeConfig.find((item) => item.path === location.pathname)
+  const isLogin = localStorage.getItem('user_info') ? true : false
+  if (result && (!result.auth || (result.auth && isLogin))) {
+    return isLogin ? (
+      <>
+        <Redirect to='/home'></Redirect>
+        <Route component={Home}></Route>
+      </>
+    ) : (
+      <Route component={result.component} pathname={result.path}></Route>
+    )
+  } else {
+    return <Redirect to='/index'></Redirect>
+  }
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Router>
+        <Switch>
+          <FrontedAuth />
+        </Switch>
+      </Router>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
